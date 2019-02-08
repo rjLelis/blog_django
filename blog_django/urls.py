b8d186b0f94bdfadc26334a21936039f4bbe8a2c
rjLelis <renatojlelis@gmail.com>
2019-02-08 14:47:50 -0200

    Add login & logout pages

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
     path('register/', users_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
 ]
