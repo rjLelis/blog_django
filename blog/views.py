c438651dd406b87a415d3c26d9ff5acac2aace28
R.J Lelis <renatojlelis@gmail.com>
2019-03-05 15:06:11 -0300

    Changed the title logic of the home page

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from .models import Post
 def home(request):
     context = {
         'posts': Post.objects.all(),
-        'isHome': True
     }
     return render(request, 'blog/home.html', context=context)
 
