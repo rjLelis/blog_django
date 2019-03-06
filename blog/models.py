66945151174967eb43bd1fd8e18bbf361833ec03
R.J Lelis <renatojlelis@gmail.com>
2019-03-06 18:57:23 -0300

    Add absolute url for the post model & link to the post detail in the home page

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
+
 
 class Post(models.Model):
 
@@ -14,3 +16,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post-detail', kwargs={'pk': self.pk})
