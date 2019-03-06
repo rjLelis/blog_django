7cbdde8c8831bb2b7eccad4d83900943ea9bfd22
R.J Lelis <renatojlelis@gmail.com>
2019-03-06 18:51:35 -0300

    Create class based views for blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
+    path('', PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('new/', PostCreateView.as_view(), name='post-new'),
     path('about',views.about, name='about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
@@ -8,6 +10,25 @@ def home(request):
     }
     return render(request, 'blog/home.html', context=context)
 
+
+class PostListView(ListView):
+    model = Post
+    template_name='blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html')
