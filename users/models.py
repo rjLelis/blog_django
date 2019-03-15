9a7c279491b0371b4e9c5ad7fa468530bc4fa04e
rjLelis <renatojlelis@gmail.com>
2019-03-15 09:34:02 -0300

    Simple code formating changes on Profile model

--- users/models.py
+++ users/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
 
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
+
     def __str__(self):
         return f'{self.user.username} Profile'
 
@@ -16,7 +17,6 @@ class Profile(models.Model):
         super().save()
 
         img = Image.open(self.image.path)
-
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
