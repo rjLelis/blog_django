1b8996f2f7cfcd6bb5cecdd6ba7c3aa42f1ae96f
rjLelis <renatojlelis@gmail.com>
2019-04-01 15:10:46 -0300

    Changed the way app are listed in settings.py

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
-    'users.apps.UsersConfig',
+    'blog',
+    'users',
     'crispy_forms',
 ]
 
