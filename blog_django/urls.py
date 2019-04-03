5c4174c777a7d38700d345bec1bbab782c2cc844
rjLelis <renatojlelis@gmail.com>
2019-04-03 12:34:36 -0300

    Add password-reset feature [WIP]

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -12,8 +12,10 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    ''' namespace='users' ''''
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,7 +23,17 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
-    path('user/', include('users.urls', namespace='users'))
+    path('user/', include('users.urls')),
+    path('password-reset/', 
+        auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
+    name="password_reset"),
+    path('password-reset/done/', 
+        auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+    name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+    name='password_reset_confirm')
+
 ]
 
 if settings.DEBUG:
