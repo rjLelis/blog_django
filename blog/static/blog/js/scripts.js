af98a4be8e483900693dbc6a4ff62c8151dfad62
rjLelis <renatojlelis@gmail.com>
2019-06-04 22:16:43 -0300

    Improve removeFeedback function

--- blog/static/blog/js/scripts.js
+++ blog/static/blog/js/scripts.js
@@ -1,1 +1,5 @@
-const removeFeedBackMessage = message => message.style.display = 'none';
+function removeFeedBackMessage(id) {
+    let feedbackMessages = document.getElementById('feedback-messages');
+    let feedbackMessage = document.getElementById(id);
+    feedbackMessages.removeChild(feedbackMessage);
+}
